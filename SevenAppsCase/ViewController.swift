048b1a7082da76f4eae30d3479bec0e0b456c24b
BarisDilekci <barixmenuygulama@gmail.com>
2025-02-14 19:34:56 +0300

    Initial Commit

---
+++ SevenAppsCase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SevenAppsCase
+//
+//  Created by Barış Dilekçi on 14.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
