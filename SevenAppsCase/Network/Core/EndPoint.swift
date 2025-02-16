ef3aff4498d457910871f0f8602e8cc272720a76
BarisDilekci <barixmenuygulama@gmail.com>
2025-02-16 14:40:50 +0300

    fix: since it is a modular structure, I added the detail section with a few lines of code

--- SevenAppsCase/Network/Core/EndPoint.swift
+++ SevenAppsCase/Network/Core/EndPoint.swift
@@ -6,51 +6,56 @@
 //
 
 /* Burasını oluşturmamın amacı url ve istek dağınıklığını bir yere toplamak. Eğer POST, DELETE ve  UPDATE işlemeri olsaydı burada method içinde yapacaktım. Böylece ViewModel içinde daha az görev olacak ve yönetmek kolay olacaktı.*/
-import Foundation
+    import Foundation
 
-protocol EndPointProtocol {
-    var baseURL: String { get }
-    var path: String { get }
-    var method: HTTPMethodType { get }
-    
-    func request() -> URLRequest
-}
-
-enum EndPoint: EndPointProtocol {
-    case fetchUserData
-
-    var baseURL: String {
-        return AppConstants.API.baseURL
+    protocol EndPointProtocol {
+        var baseURL: String { get }
+        var path: String { get }
+        var method: HTTPMethodType { get }
+        
+        func request() -> URLRequest
     }
 
-    var path: String {
-        switch self {
-        case .fetchUserData:
-            return AppConstants.API.path
+    enum EndPoint: EndPointProtocol {
+        case fetchUserData
+        case fetchUserDetail(id: Int)
+
+        var baseURL: String {
+            return AppConstants.API.baseURL
+        }
+
+        var path: String {
+            switch self {
+            case .fetchUserData:
+                return AppConstants.API.path
+            case .fetchUserDetail(id: let id ):
+                return "\(AppConstants.API.path)/\(id)"
+            }
+        }
+
+        var method: HTTPMethodType {
+            switch self {
+            case .fetchUserData:
+                return .get
+            case .fetchUserDetail(id: let id ):
+                return .get
+            }
+        }
+
+        func request() -> URLRequest {
+            guard var urlComponents = URLComponents(string: baseURL) else {
+                fatalError("Geçersiz baseURL: \(baseURL)")
+            }
+
+            urlComponents.path = path
+
+            guard let url = urlComponents.url else {
+                fatalError("URL oluşturulamadı: \(urlComponents)")
+            }
+
+            var request = URLRequest(url: url)
+            request.httpMethod = method.rawValue
+
+            return request
         }
     }
-
-    var method: HTTPMethodType {
-        switch self {
-        case .fetchUserData:
-            return .get
-        }
-    }
-
-    func request() -> URLRequest {
-        guard var urlComponents = URLComponents(string: baseURL) else {
-            fatalError("Geçersiz baseURL: \(baseURL)")
-        }
-
-        urlComponents.path = path
-
-        guard let url = urlComponents.url else {
-            fatalError("URL oluşturulamadı: \(urlComponents)")
-        }
-
-        var request = URLRequest(url: url)
-        request.httpMethod = method.rawValue
-
-        return request
-    }
-}
