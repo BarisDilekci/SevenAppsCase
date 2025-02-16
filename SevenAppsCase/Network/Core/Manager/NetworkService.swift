38b87265a7836e5a0dcf2a376f83744e4f6a4ad9
BarisDilekci <barixmenuygulama@gmail.com>
2025-02-16 14:51:17 +0300

    fix: A new function was created in the network service for user detail

--- SevenAppsCase/Network/Core/Manager/NetworkService.swift
+++ SevenAppsCase/Network/Core/Manager/NetworkService.swift
@@ -11,19 +11,35 @@
 import Foundation
 
 protocol INetworkService {
-    func fetchUserData(completion: @escaping (Result<User, Error>) -> Void)
+    func fetchUserData(completion: @escaping (Result<[User], Error>) -> Void)
+    func fetchUserDetailData(id: Int, completion: @escaping (Swift.Result<User, Error>) -> ())
 }
 
 final class NetworkService: INetworkService {
-    
     private let networkManager: INetworkManager
-
+    
     init(networkManager: INetworkManager = NetworkManager()) {
         self.networkManager = networkManager
     }
-
-    func fetchUserData(completion: @escaping (Result<User, Error>) -> Void) {
+    
+    func fetchUserData(completion: @escaping (Result<[User], Error>) -> Void) {
         let endPoint = EndPoint.fetchUserData
         networkManager.request(endPoint, completion: completion)
     }
+    
+    func fetchUserDetailData(id: Int, completion: @escaping (Result<User, any Error>) -> ()) {
+        let endpoint = EndPoint.fetchUserDetail(id: id)
+        
+        networkManager.request(endpoint) { (result: Result<User, Error>) in
+            switch result {
+            case .success(let userDetail):
+                print("Fetched User Detail: \(userDetail)")
+                completion(.success(userDetail))
+            case .failure(let error):
+                print("Error fetching User detail: \(error.localizedDescription)")
+                completion(.failure(error))
+            }
+        }
+        
+    }  
 }
